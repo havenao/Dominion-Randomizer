237b6e8a6c8eb0a859c2051d7984691d430212c6
Haven Schrock <haven@Havens-MacBook-Pro.local>
2021-01-25 15:39:12 -0500

    testing deploy. had to add . before filepath?

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import React from "react";
 const Card = ({ card }) => {
   return (
     <div className="card item">
-      <img src={`/images/${card.name.toLowerCase()}.jpg`} alt={card.name} />
+      <img src={`./images/${card.name.toLowerCase()}.jpg`} alt={card.name} />
       <h4 className="overlay-text">{card.name}</h4>
     </div>
   );
--- src/Extra.js
+++ src/Extra.js
@@ -6,7 +6,7 @@ const Extra = ({ extra }) => {
       <div className={`${extra.type} extra-label`}>
         <h4>{extra.type}</h4>
       </div>
-      <img src={`/images/${extra.name.toLowerCase()}.jpg`} alt={extra.name} />
+      <img src={`./images/${extra.name.toLowerCase()}.jpg`} alt={extra.name} />
       <h4 className="overlay-text">{extra.name}</h4>
     </div>
   );
